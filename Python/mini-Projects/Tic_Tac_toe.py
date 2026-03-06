64d65ba715b65f0d4837101b47c81138981e528f
Aadarshanpal <aadarshanpal@gmail.com>
2026-03-06 14:00:52 +0545

    Two player tic tac toe Complete

--- Python/mini-Projects/Tic_Tac_toe.py
+++ Python/mini-Projects/Tic_Tac_toe.py
@@ -4,8 +4,9 @@ board = ['-','-','-',
 
 
 turn = 'O'
-game_over = False
 
+print("The board is indexed from position 0 to 8, the game ends when a winning condition or draw is met.")
+print("---------------------------------------------------------------\n")
 
 #-------------------DRAWING THE BOARD----------------------------
 def draw_board(board):
@@ -22,6 +23,7 @@ def draw_check(board):
     return True
 
 
+#-------------------INPUT HANDLING-------------------------------
 def take_input(turn,board):
     print("Turn:",turn)
     while True:
@@ -43,14 +45,30 @@ def take_input(turn,board):
     else:
         return 'O'
 
-while not game_over:
-    draw_board(board)
 
+def win_check(board):
+    win_patterns = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+
+    for pattern in win_patterns:
+        if board[pattern[0]] == board[pattern[1]] == board[pattern[2]] != '-':
+            return True
+    
+    return False
+
+#-----------------Main game loop---------------------------
+while True:
+    draw_board(board)
+    print("---------------------------------------------------------------")
+    temp_turn = turn
     turn = take_input(turn,board)
-    #check if someone has won
+    print("---------------------------------------------------------------")
+    if win_check(board):
+        draw_board(board)
+        print("The Game has been won By:",temp_turn)
+        break
     if draw_check(board):
         draw_board(board)
         print("The game is a draw.")
         break
 
-    print("---------------------------------------------------------------\n\n\n")
+    
